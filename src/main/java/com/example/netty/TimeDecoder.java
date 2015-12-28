d7f322470fdb4f4b67f7809736ad275e8e54b8c0
alexander <alexander.karpoff@gmail.com>
2015-12-28 08:04:49 +0300

    playing with netty

--- src/main/java/com/example/netty/TimeDecoder.java
+++ src/main/java/com/example/netty/TimeDecoder.java
@@ -2,22 +2,18 @@ package com.example.netty;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.ReplayingDecoder;
 
 import java.util.List;
 
 /**
  * Created by alexander on 12/28/15.
  */
-public class TimeDecoder extends ByteToMessageDecoder {
+public class TimeDecoder extends ReplayingDecoder<Void> {
 
     @Override
     protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf in, List<Object> out) throws Exception {
-        if (in.readableBytes() < 4) {
-            return; // (3)
-        }
-
-        out.add(in.readBytes(4)); // (4)
+        out.add(in.readBytes(4));
     }
 
 }
